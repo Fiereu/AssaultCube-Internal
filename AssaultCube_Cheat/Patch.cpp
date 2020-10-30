5d3c769ce385da5c386e23bd3bd8e2df1138b58c
Fiereu <32392522+Fiereu@users.noreply.github.com>
2020-10-30 09:17:18 +0100

    Update Patch.cpp

--- AssaultCube_Cheat/Patch.cpp
+++ AssaultCube_Cheat/Patch.cpp
@@ -1,10 +1,10 @@
 #include "Patch.h"
 
 
-Patch::Patch(int Adress, std::vector<int> orgBytes, int size, std::vector<int> patchBytes)
+Patch::Patch(int Address, std::vector<int> orgBytes, int size, std::vector<int> patchBytes)
 {
-	std::cout << "[+] Patch Created for " << std::hex << Adress << std::endl;
-	gAdress = Adress;
+	std::cout << "[+] Patch Created for " << std::hex << Address << std::endl;
+	gAddress = Address;
 	gSize = size;
 	gOrgBytes = orgBytes;
 	gPatchBytes = patchBytes;
@@ -12,27 +12,27 @@ Patch::Patch(int Adress, std::vector<int> orgBytes, int size, std::vector<int> p
 
 Patch::~Patch(void)
 {
-	std::cout << "[+] Patch Destroyed at " << std::hex << gAdress << " TOGGLED = " + gToggled << std::endl;
+	std::cout << "[+] Patch Destroyed at " << std::hex << gAddress << " TOGGLED = " + gToggled << std::endl;
 }
 
 LONG Patch::Toggle()
 {
 	LONG STATUS = ERROR_SUCCESS;
 	DWORD OldProtect;
-	VirtualProtect((LPVOID)gAdress, gSize, PAGE_EXECUTE_READWRITE, &OldProtect);
+	VirtualProtect((LPVOID)gAddress, gSize, PAGE_EXECUTE_READWRITE, &OldProtect);
 	if (gToggled) {
 		for (int i = 0; i < gSize; i++) {
-			memset((void*)(gAdress + i), gOrgBytes.at(i), 1);
+			memset((void*)(gAddress + i), gOrgBytes.at(i), 1);
 		}
 		gToggled = !gToggled;
 	}
 	else {
 		for (int i = 0; i < gSize; i++) {
-			memset((void*)(gAdress + i), gPatchBytes.at(i), 1);
+			memset((void*)(gAddress + i), gPatchBytes.at(i), 1);
 		}
 		gToggled = !gToggled;
 	}
-	VirtualProtect((LPVOID)gAdress, gSize, OldProtect, nullptr);
+	VirtualProtect((LPVOID)gAddress, gSize, OldProtect, nullptr);
 	return STATUS;
 }
 
